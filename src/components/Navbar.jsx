d36642a5fb40e688217551e0f2b57ad54fd40b4b
Ahmed <ahmedmahmud1994@hotmail.com>
2024-09-17 15:48:07 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaHome } from "react-icons/fa"; 
+
+const Navbar = () => {
+    return (
+        <nav>
+            NestQuest Estate
+        </nav>
+    )
+}
+
+export default Navbar
