d138caff536a3655c40f4725647e35562292e956
Ahmed <ahmedmahmud1994@hotmail.com>
2024-09-18 10:33:41 +0100

    navbar active state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Outlet />
+      </Layout>
+    ),
+    errorElement: <div>No Such Route Exists!!!</div>,
+    children: [
+      {
+        index: true,
+        element: <Explore />,
+      },
+      {
+        path: 'offers',
+        element: <Offers />
+      },
+      {
+        path: 'profile',
+        element: <Profile />
+      }
+    ],
+  },
+]);
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-      
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <Navbar />
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRight } from '../assets/svg/keyboardArrowRightIcon.svg'
+import VisibilityIcon from '../assets/svg/visibilityIcon.svg'
+
+const Signin = () => {
+  return (
+    <form action="">
+
+    </form>
+  )
+}
+
+export default Signin
